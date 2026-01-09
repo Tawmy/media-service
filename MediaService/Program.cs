8801913a18e29700720ad06394d75faf6479f9d8
Tawmy <git@tawmy.dev>
2026-01-09 10:49:59 +0100

    Initial commit

---
+++ MediaService/Program.cs
@@ -0,0 +1,32 @@
+using MediaService.Components;
+using _Imports = MediaService.Client._Imports;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", true);
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(_Imports).Assembly);
+
+app.Run();
